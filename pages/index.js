b3c61e4a8ad8255c14fb1e5379e741c30fe8505e
Lucas Delatron <delatron.lucas@gmail.com>
2023-05-04 13:32:18 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,50 @@
+import { createClient } from 'next-sanity'
+
 export default function IndexPage({ pets }) {
-  return (
-    <>
-      <header>
-        <h1>Sanity + Next.js</h1>
-      </header>
-      <main>
-        <h2>Pets</h2>
-        {pets.length > 0 && (
-          <ul>
-            {pets.map((pet) => (
-              <li key={pet._id}>{pet?.name}</li>
-            ))}
-          </ul>
-        )}
-        {!pets.length > 0 && <p>No pets to show</p>}
-        {pets.length > 0 && (
-          <div>
-            <pre>{JSON.stringify(pets, null, 2)}</pre>
-          </div>
-        )}
-        {!pets.length > 0 && (
-          <div>
-            <div>¯\_(ツ)_/¯</div>
-            <p>
-              Your data will show up here when you've configured everything
-              correctly
-            </p>
-          </div>
-        )}
-      </main>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<h1>Sanity + Next.js</h1>
+			</header>
+			<main>
+				<h2>Pets</h2>
+				{pets.length > 0 && (
+					<ul>
+						{pets.map((pet) => (
+							<li key={pet._id}>{pet?.name}</li>
+						))}
+					</ul>
+				)}
+				{!pets.length > 0 && <p>No pets to show</p>}
+				{pets.length > 0 && (
+					<div>
+						<pre>{JSON.stringify(pets, null, 2)}</pre>
+					</div>
+				)}
+				{!pets.length > 0 && (
+					<div>
+						<div>¯\_(ツ)_/¯</div>
+						<p>Your data will show up here when you've configured everything correctly</p>
+					</div>
+				)}
+			</main>
+		</>
+	)
 }
 
-export async function getStaticProps() {
-  const pets = [
-    /* {
-      _createdAt: "2022-03-08T09:28:00Z",
-      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-      _rev: "xnBg0xhUDzo561jnWODd5e",
-      _type: "pet",
-      _updatedAt: "2022-03-08T09:28:00Z",
-      name: "Bamse"
-    } */
-  ];
+const client = createClient({
+	projectId: '7w2c0doi',
+	dataset: 'production',
+	apiVersion: '2023-04-04',
+	useCdn: false,
+})
 
-  return {
-    props: {
-      pets
-    }
-  };
+export async function getStaticProps() {
+	const pets = await client.fetch(`*[_type == "pet"]`)
+
+	return {
+		props: {
+			pets,
+		},
+	}
 }
